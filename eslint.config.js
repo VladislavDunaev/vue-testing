b84122adfac710d0c21ae89f2fc294c8e330b5b3
Vladislav Dunaiev <v.dunaiev@urancompany.com>
2024-07-28 11:11:59 +0300

    Update dependency versions

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintVue from "eslint-plugin-vue";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import vueParser from "vue-eslint-parser";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default tseslint.config(
+    {
+        plugins: {
+            "@typescript-eslint": tseslint.plugin,
+            vue: eslintVue,
+            prettier: prettierPlugin,
+        },
+    },
+    {
+        ignores: ["node_modules"],
+    },
+    js.configs.recommended,
+    ...eslintVue.configs["flat/essential"],
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2021,
+            },
+            parser: vueParser,
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+            ecmaVersion: "latest",
+        },
+    },
+    {
+        files: ["**/*.{js,ts,vue}"],
+        rules: {
+            ...eslintConfigPrettier.rules,
+            "prettier/prettier": ["warn"],
+            "prefer-const": "warn",
+        },
+    },
+);
