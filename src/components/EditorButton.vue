f85e1403c50d4b3be26eb5b2a9fb619022eec058
Vladislav Dunaiev <v.dunaiev@urancompany.com>
2024-07-31 17:13:39 +0300

    Refactor editor view

---
+++ src/components/EditorButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button
+        @click="$emit('click')"
+        type="button"
+        class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
+    >
+        <slot></slot>
+        <span class="sr-only">{{ title }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "EditorButton",
+    emits: {
+        click: null,
+    },
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -5,140 +5,38 @@
                 <div class="flex items-center justify-between px-3 py-2 border-b">
                     <div class="flex flex-wrap items-center divide-gray-200 sm:divide-x sm:rtl:divide-x-reverse">
                         <div class="flex items-center space-x-1 rtl:space-x-reverse sm:pe-3">
-                            <button
-                                @click="applyList('ordered')"
-                                type="button"
-                                class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                            >
-                                <svg
-                                    class="w-4 h-4"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 21 18"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M9.5 3h9.563M9.5 9h9.563M9.5 15h9.563M1.5 13a2 2 0 1 1 3.321 1.5L1.5 17h5m-5-15 2-1v6m-2 0h4"
-                                    />
-                                </svg>
-                                <span class="sr-only">Ordered list</span>
-                            </button>
+                            <editor-button @click="applyList('ordered')" title="Ordered list">
+                                <img width="16" height="16" src="@/assets/icons/ordered-list.svg" alt="Ordered List" />
+                            </editor-button>
                         </div>
                         <div class="flex flex-wrap items-center space-x-1 rtl:space-x-reverse sm:pe-3 sm:ps-3">
-                            <button
-                                @click="applyList('unordered')"
-                                type="button"
-                                class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                            >
-                                <svg
-                                    class="w-6 h-6 text-gray-800"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
+                            <editor-button @click="applyList('unordered')" title="Unordered list">
+                                <img
                                     width="24"
                                     height="24"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-width="2"
-                                        d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"
-                                    />
-                                </svg>
-
-                                <span class="sr-only">Unordered List</span>
-                            </button>
+                                    src="@/assets/icons/unordered-list.svg"
+                                    alt="Unordered List"
+                                />
+                            </editor-button>
                         </div>
                         <div class="flex flex-wrap items-center space-x-1 rtl:space-x-reverse sm:pe-3 sm:ps-3">
-                            <button
-                                @click="applyStyle('bold')"
-                                type="button"
-                                class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                            >
-                                <svg
-                                    class="w-6 h-6 text-gray-800"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M8 5h4.5a3.5 3.5 0 1 1 0 7H8m0-7v7m0-7H6m2 7h6.5a3.5 3.5 0 1 1 0 7H8m0-7v7m0 0H6"
-                                    />
-                                </svg>
-
-                                <span class="sr-only">Text Bold</span>
-                            </button>
+                            <editor-button @click="applyStyle('bold')" title="Text Bold">
+                                <img width="24" height="24" src="@/assets/icons/text-bold.svg" alt="Text Bold" />
+                            </editor-button>
                         </div>
                         <div class="flex flex-wrap items-center space-x-1 rtl:space-x-reverse sm:pe-3 sm:ps-3">
-                            <button
-                                @click="applyStyle('italic')"
-                                type="button"
-                                class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                            >
-                                <svg
-                                    class="w-6 h-6 text-gray-800"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="m8.874 19 6.143-14M6 19h6.33m-.66-14H18"
-                                    />
-                                </svg>
-
-                                <span class="sr-only">Text Italics</span>
-                            </button>
+                            <editor-button @click="applyStyle('italic')" title="Text Italics">
+                                <img width="24" height="24" src="@/assets/icons/text-italic.svg" alt="Text Italic" />
+                            </editor-button>
                         </div>
                         <div class="flex flex-wrap items-center space-x-1 rtl:space-x-reverse sm:pe-3 sm:ps-3">
-                            <button
-                                @click="applyLink"
-                                type="button"
-                                class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100"
-                            >
-                                <svg
-                                    class="w-6 h-6 text-gray-800"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"
-                                    />
-                                </svg>
-
-                                <span class="sr-only">Add Link</span>
-                            </button>
+                            <editor-button @click="applyLink" title="Add Link">
+                                <img width="24" height="24" src="@/assets/icons/link.svg" alt="Add Link" />
+                            </editor-button>
                         </div>
                     </div>
                 </div>
                 <div class="px-4 py-2 bg-white rounded-b-lg">
-                    <label for="editor" class="sr-only">Publish post</label>
                     <div
                         id="editor"
                         ref="editor"
@@ -157,7 +55,7 @@
                 Save To LS
             </button>
             <button
-                @click="retriveFromLocalStorage"
+                @click="retrieveFromLocalStorage"
                 type="button"
                 class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg focus:ring-4 focus:ring-blue-200"
             >
@@ -168,34 +66,41 @@
 </template>
 
 <script lang="ts">
+import type { IListStyle, ITextStyle } from "@/types";
+import { ORDERED_LIST, TEXT_BOLD, TEXT_ITALIC, TEXT_NORMAL } from "@/constants";
+import EditorButton from "@/components/EditorButton.vue";
+
 export default {
+    components: { EditorButton },
     methods: {
-        applyStyle(style) {
+        applyStyle(style: ITextStyle) {
             const selection = window.getSelection();
-            if (selection.rangeCount) {
+            if (selection?.rangeCount) {
                 const range = selection.getRangeAt(0);
-                const selectedNode = range.startContainer.parentNode;
+                const selectedNode = range.startContainer.parentNode as HTMLElement;
                 if (
-                    (style === "bold" && selectedNode.style.fontWeight === "bold") ||
-                    (style === "italic" && selectedNode.style.fontStyle === "italic")
+                    (style === TEXT_BOLD && selectedNode.style.fontWeight === TEXT_BOLD) ||
+                    (style === TEXT_ITALIC && selectedNode.style.fontStyle === TEXT_ITALIC)
                 ) {
                     const parent = selectedNode.parentNode;
-                    while (selectedNode.firstChild) parent.insertBefore(selectedNode.firstChild, selectedNode);
-                    parent.removeChild(selectedNode);
+                    while (selectedNode.firstChild) parent?.insertBefore(selectedNode.firstChild, selectedNode);
+                    parent?.removeChild(selectedNode);
                 } else {
                     const span = document.createElement("span");
-                    span.style.fontWeight = style === "bold" ? "bold" : "normal";
-                    span.style.fontStyle = style === "italic" ? "italic" : "normal";
+                    span.style.fontWeight = style === TEXT_BOLD ? TEXT_BOLD : TEXT_NORMAL;
+                    span.style.fontStyle = style === TEXT_ITALIC ? TEXT_ITALIC : TEXT_NORMAL;
                     range.surroundContents(span);
                 }
             }
+
+            selection?.removeAllRanges();
         },
-        applyList(listType) {
+        applyList(listType: IListStyle) {
             const selection = window.getSelection();
-            if (selection.rangeCount) {
+            if (selection?.rangeCount) {
                 const range = selection.getRangeAt(0);
                 const selectedText = selection.toString();
-                const list = document.createElement(listType === "ordered" ? "ol" : "ul");
+                const list = document.createElement(listType === ORDERED_LIST ? "ol" : "ul");
 
                 selectedText.split("\n").forEach((line) => {
                     if (line.trim() !== "") {
@@ -213,22 +118,24 @@ export default {
             const url = prompt("Enter the URL");
             if (url) {
                 const selection = window.getSelection();
-                if (selection.rangeCount) {
+                if (selection?.rangeCount) {
                     const range = selection.getRangeAt(0);
                     const anchor = document.createElement("a");
                     anchor.href = url;
                     anchor.target = "_blank";
-                    range.surroundContents(anchor);
+                    anchor.textContent = selection.toString();
+                    range.deleteContents();
+                    range.insertNode(anchor);
                 }
             }
         },
         saveToLocalStorage() {
-            localStorage.setItem("content", this.$refs["editor"].innerHTML);
+            localStorage.setItem("content", (this.$refs["editor"] as HTMLElement).innerHTML);
         },
-        retriveFromLocalStorage() {
+        retrieveFromLocalStorage() {
             const savedContent = localStorage.getItem("content");
             if (savedContent) {
-                this.$refs["editor"].innerHTML = savedContent;
+                (this.$refs["editor"] as HTMLElement).innerHTML = savedContent;
             }
         },
     },
